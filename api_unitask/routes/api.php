722e9a6b829aae2cc4ebd8f3c08fac7d315e2e42
Ariel <108147166+Arielo16@users.noreply.github.com>
2025-01-27 17:14:11 -0600

    route update

--- api_unitask/routes/api.php
+++ api_unitask/routes/api.php
@@ -39,41 +39,41 @@ Route::prefix('goods')->group(function () {
 });
 
 Route::prefix('categories')->group(function () {
-    Route::get('/', [CategoryController::class, 'index']); // Obtener todas las categorías
-    Route::post('/', [CategoryController::class, 'store']); // Crear una nueva categoría
-    Route::get('/{category_id}', [CategoryController::class, 'show']); // Obtener una categoría específica
-    Route::put('/{category_id}', [CategoryController::class, 'update']); // Actualizar una categoría
-    Route::delete('/{category_id}', [CategoryController::class, 'destroy']); // Eliminar una categoría
+    Route::get('get/', [CategoryController::class, 'index']); // Obtener todas las categorías
+    Route::post('post/', [CategoryController::class, 'store']); // Crear una nueva categoría
+    Route::get('get/{category_id}', [CategoryController::class, 'show']); // Obtener una categoría específica
+    Route::put('put/{category_id}', [CategoryController::class, 'update']); // Actualizar una categoría
+    Route::delete('delete/{category_id}', [CategoryController::class, 'destroy']); // Eliminar una categoría
 });
 
 Route::prefix('rooms')->group(function () {
-    Route::get('/', [RoomController::class, 'index']); // Obtener todos los salones
-    Route::post('/', [RoomController::class, 'store']); // Crear un nuevo salón
-    Route::get('/{room_id}/{building_id}', [RoomController::class, 'show']); // Obtener un salón específico
-    Route::put('/{room_id}/{building_id}', [RoomController::class, 'update']); // Actualizar un salón
-    Route::delete('/{room_id}/{building_id}', [RoomController::class, 'destroy']); // Eliminar un salón
+    Route::get('get/', [RoomController::class, 'index']); // Obtener todos los salones
+    Route::post('post/', [RoomController::class, 'store']); // Crear un nuevo salón
+    Route::get('get/{room_id}/{building_id}', [RoomController::class, 'show']); // Obtener un salón específico
+    Route::put('put/{room_id}/{building_id}', [RoomController::class, 'update']); // Actualizar un salón
+    Route::delete('delete/{room_id}/{building_id}', [RoomController::class, 'destroy']); // Eliminar un salón
 });
 
 Route::prefix('diagnostics')->group(function () {
-    Route::get('/', [DiagnosticController::class, 'index']); // Obtener todos los diagnósticos
-    Route::post('/', [DiagnosticController::class, 'store']); // Crear un diagnóstico
-    Route::get('/{id}', [DiagnosticController::class, 'show']); // Obtener un diagnóstico específico
-    Route::put('/{id}', [DiagnosticController::class, 'update']); // Actualizar un diagnóstico
-    Route::delete('/{id}', [DiagnosticController::class, 'destroy']); // Eliminar un diagnóstico
+    Route::get('get/', [DiagnosticController::class, 'index']); // Obtener todos los diagnósticos
+    Route::post('post/', [DiagnosticController::class, 'store']); // Crear un diagnóstico
+    Route::get('get/{id}', [DiagnosticController::class, 'show']); // Obtener un diagnóstico específico
+    Route::put('put/{id}', [DiagnosticController::class, 'update']); // Actualizar un diagnóstico
+    Route::delete('delete/{id}', [DiagnosticController::class, 'destroy']); // Eliminar un diagnóstico
 });
 
 Route::prefix('users')->group(function () {
-    Route::get('/', [UserController::class, 'index']); // Obtener todos los usuarios
-    Route::post('/', [UserController::class, 'store']); // Crear un usuario
-    Route::get('/{matricula}', [UserController::class, 'show']); // Obtener un usuario específico
-    Route::put('/{matricula}', [UserController::class, 'update']); // Actualizar un usuario
-    Route::delete('/{matricula}', [UserController::class, 'destroy']); // Eliminar un usuario
+    Route::get('get/', [UserController::class, 'index']); // Obtener todos los usuarios
+    Route::post('post/', [UserController::class, 'store']); // Crear un usuario
+    Route::get('get/{matricula}', [UserController::class, 'show']); // Obtener un usuario específico
+    Route::put('put/{matricula}', [UserController::class, 'update']); // Actualizar un usuario
+    Route::delete('delete/{matricula}', [UserController::class, 'destroy']); // Eliminar un usuario
 });
 
 Route::prefix('types')->group(function () {
-    Route::get('/', [TypeController::class, 'index']); // Obtener todos los tipos
-    Route::post('/', [TypeController::class, 'store']); // Crear un tipo
-    Route::get('/{typeID}', [TypeController::class, 'show']); // Obtener un tipo específico
-    Route::put('/{typeID}', [TypeController::class, 'update']); // Actualizar un tipo
-    Route::delete('/{typeID}', [TypeController::class, 'destroy']); // Eliminar un tipo
+    Route::get('get/', [TypeController::class, 'index']); // Obtener todos los tipos
+    Route::post('post/', [TypeController::class, 'store']); // Crear un tipo
+    Route::get('get/{typeID}', [TypeController::class, 'show']); // Obtener un tipo específico
+    Route::put('put/{typeID}', [TypeController::class, 'update']); // Actualizar un tipo
+    Route::delete('delete/{typeID}', [TypeController::class, 'destroy']); // Eliminar un tipo
 });
