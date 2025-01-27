889d3b949b99a0f9af0eba6309f3f0c401fc98d1
Ariel <108147166+Arielo16@users.noreply.github.com>
2025-01-27 12:50:09 -0600

    Update con todo y datos

--- api_unitask/routes/api.php
+++ api_unitask/routes/api.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\{
 // Definición de las rutas
 Route::prefix('reports')->group(function () {
     Route::get('get/', [ReportController::class, 'index']);
+    Route::get('todo/', [ReportController::class, 'todo']);
     Route::post('post/', [ReportController::class, 'store']);
     Route::get('get/{folio}', [ReportController::class, 'show']);
     Route::put('put/{folio}', [ReportController::class, 'update']);
